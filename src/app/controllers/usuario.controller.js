9793455b0a719a729a1dce33bd6b0ed8f92d782e
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-23 16:55:28 -0300

    refatora UsuarioController para incluir método de criação de usuário e validação

--- src/app/controllers/usuario.controller.js
+++ src/app/controllers/usuario.controller.js
@@ -1,15 +1,37 @@
 import usuarioService from "../services/usuario.service.js";
+import { validarCriacaoUsuario } from "../middlewares/validations/usuario.validation.js";
 
 class UsuarioController{
 
-    async findAll(req, res){
+    constructor(){
+        this.usuarioService = usuarioService;
+    }
+
+    create = async (req, res) => {
+        try {
+            const {nome, email, senha} = req.body;
+
+            validarCriacaoUsuario(nome, email, senha);
+
+            const usuario = await this.usuarioService.create({nome, email, senha});
+
+            res.status(201).json(usuario);
+
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
+        }
+    }
+
+    findAll = async (req, res) => {
         try {
             const usuarios = await usuarioService.findAll();
             res.json(usuarios);
         } catch (error) {
             res.status(500).json({ error: "Erro ao buscar usuários" });
         }
-}
+    }
+
 }
 
 export default new UsuarioController();
+
