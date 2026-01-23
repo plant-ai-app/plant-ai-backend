0db6ecf63e53efea76d98f54946cd3b809f54cd9
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-23 16:56:10 -0300

    refatora UsuarioService para utilizar arrow functions e melhorar a legibilidade

--- src/app/services/usuario.service.js
+++ src/app/services/usuario.service.js
@@ -2,15 +2,39 @@ import usuarioRepository from "../repositories/usuario.repository.js";
 
 
 class UsuarioService{
-    async findAll(){
-        const usuarios = await usuarioRepository.findAll();
 
+    constructor(){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    findAll = async () => {
+        const usuarios = await this.usuarioRepository.findAll();
         if(!usuarios || usuarios.length === 0){
             return [];
         }
 
         return usuarios;
     }
+
+    create = async ({nome, email, senha}) => {
+        //email unico
+        const usuarioExistente = await this.usuarioRepository.findByEmail(email);
+
+        if(usuarioExistente){
+            throw new Error("Já existe um usuário cadastrado com esse e-mail.");
+        }
+
+        const usuario = await this.usuarioRepository.create({
+            nome,
+            email,
+            senha_hash: senha
+        })
+
+        return usuario;
+
+    }
+
+
 }
 
 export default new UsuarioService();
