9ae8d266563a441137e14c33d17182dcc9e67afa
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-26 15:55:54 -0300

    adiciona middleware de autenticação com verificação de token JWT

---
+++ src/app/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Token não fornecido.' });
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.usuarioId = decoded.id;
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: 'Token inválido.' });
+    }
+}
