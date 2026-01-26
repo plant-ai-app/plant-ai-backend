c0346073c7433a16fe1bec506dd3d08bbeff00a3
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-26 15:55:17 -0300

    organiza a ordem das importações e melhora a legibilidade do arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import express from 'express';
+import 'dotenv/config';
 import cors from 'cors';
 import {Routes} from './app/routes/index.routes.js';
 
 const app = express();
-Routes(app);
-
 app.use(express.json());
+
 app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
 }));
 
-
+Routes(app);
+    
 export default app;
