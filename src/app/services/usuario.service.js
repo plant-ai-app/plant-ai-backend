e60a35a2da469c7921f80db65642433671a2e1ea
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-26 15:59:05 -0300

    refatora código anterior e adiciona métodos de login e delete

--- src/app/services/usuario.service.js
+++ src/app/services/usuario.service.js
@@ -1,14 +1,13 @@
 import usuarioRepository from "../repositories/usuario.repository.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 
 class UsuarioService{
 
-    constructor(){
-        this.usuarioRepository = usuarioRepository;
-    }
 
     findAll = async () => {
-        const usuarios = await this.usuarioRepository.findAll();
+        const usuarios = await usuarioRepository.findAll();
         if(!usuarios || usuarios.length === 0){
             return [];
         }
@@ -18,22 +17,64 @@ class UsuarioService{
 
     create = async ({nome, email, senha}) => {
         //email unico
-        const usuarioExistente = await this.usuarioRepository.findByEmail(email);
+        const usuarioExistente = await usuarioRepository.findByEmail(email);
 
         if(usuarioExistente){
             throw new Error("Já existe um usuário cadastrado com esse e-mail.");
         }
+        const senhaHash = await bcrypt.hash(senha, 10);
 
-        const usuario = await this.usuarioRepository.create({
+        const usuario = await usuarioRepository.create({
             nome,
             email,
-            senha_hash: senha
+            senha_hash: senhaHash
         })
 
         return usuario;
 
     }
 
+    login = async (email, senha) => {
+
+        // verificar se o usuario existe
+        const usuario = await usuarioRepository.findByEmail(email);
+
+        if(!usuario){
+            throw new Error("E-mail ou senha inválidos.");
+        }
+
+        const senhaValida = await bcrypt.compare(senha, usuario.senha_hash);
+
+        if(!senhaValida){
+            throw new Error("E-mail ou senha inválidos.");
+        }
+
+        const token = jwt.sign(
+            {id: usuario.id, email: usuario.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '30d'}
+        )
+
+        return {
+            token,
+            usuario
+        };
+    }
+    delete = async ({ usuarioIdLogado, usuarioIdAlvo }) => {
+        
+        if (usuarioIdLogado !== usuarioIdAlvo) {
+            throw new Error("Você não tem permissão para excluir este usuário.");
+        }
+
+        const user = await usuarioRepository.findById(usuarioIdAlvo);
+
+        if (!user) {
+            throw new Error("Usuário não encontrado.");
+        }
+
+
+        return await usuarioRepository.delete(usuarioIdAlvo);
+    };
 
 }
 
