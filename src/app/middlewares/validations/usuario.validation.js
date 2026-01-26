618c8cfd70c6f2b95eeb14d062d9534890e18075
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-26 15:56:08 -0300

    adiciona validação para login de usuário

--- src/app/middlewares/validations/usuario.validation.js
+++ src/app/middlewares/validations/usuario.validation.js
@@ -12,3 +12,13 @@ export const validarCriacaoUsuario = (name, email, senha) => {
         throw new Error("A senha deve ter no mínimo 6 caracteres.");
     }
 }
+
+export const validarLoginUsuario = (email, senha) => {
+
+    email = email?.trim().toLowerCase();
+    senha = senha?.trim();
+
+    if (!email || !senha) {
+        throw new Error("E-mail e senha são obrigatórios.");
+    }
+}
