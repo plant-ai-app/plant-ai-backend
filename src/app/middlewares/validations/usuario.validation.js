dd3fee46060e28bce031522625ae761ec28f6310
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-26 16:53:38 -0300

    adiciona validações para atualização de usuário

--- src/app/middlewares/validations/usuario.validation.js
+++ src/app/middlewares/validations/usuario.validation.js
@@ -22,3 +22,23 @@ export const validarLoginUsuario = (email, senha) => {
         throw new Error("E-mail e senha são obrigatórios.");
     }
 }
+
+export const validarAtualizacaoUsuario = (data) =>{
+    const camposPermitidos = ["nome", "email", "fk_foto_perfil"];
+    const camposRecebidos = Object.keys(data);
+
+    const camposInvalidos = camposRecebidos.filter(campo => !camposPermitidos.includes(campo));
+
+    if (camposInvalidos.length > 0) {
+        throw new Error(`Campos inválidos para atualização: ${camposInvalidos.join(", ")}`);
+    }
+
+    if(data.nome && data.nome.length < 3){
+        throw new Error("O nome deve ter no mínimo 3 caracteres.");
+    }
+    if (data.email && !data.email.includes("@")) {
+        throw new Error("Email inválido");
+    }
+
+
+}
