8853511a958dd593915ab974c31d049884c5fa98
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-26 15:58:15 -0300

    adiciona middleware de autenticação e ajusta rotas de usuário

--- src/app/routes/usuario.routes.js
+++ src/app/routes/usuario.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import usuarioController from "../controllers/usuario.controller.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.get("/usuarios", usuarioController.findAll);
-router.post("/usuarios/", usuarioController.create);
+router.post("/usuario", usuarioController.create);
+router.post("/usuario/login",usuarioController.login);
+router.delete("/usuario/:id", authMiddleware, usuarioController.delete);
+
 
     
 export default router;
