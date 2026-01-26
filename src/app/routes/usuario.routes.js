ae2be9a0d7379743b2b8a7df540c4e6e704ba962
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-26 16:54:52 -0300

    adiciona rotas para atualização de usuário e atualização de senha

--- src/app/routes/usuario.routes.js
+++ src/app/routes/usuario.routes.js
@@ -8,7 +8,8 @@ router.get("/usuarios", usuarioController.findAll);
 router.post("/usuario", usuarioController.create);
 router.post("/usuario/login",usuarioController.login);
 router.delete("/usuario/:id", authMiddleware, usuarioController.delete);
-
+router.patch("/usuario/:id", authMiddleware, usuarioController.update);
+router.patch("/usuario/:id/senha", usuarioController.updateSenha);
 
     
 export default router;
