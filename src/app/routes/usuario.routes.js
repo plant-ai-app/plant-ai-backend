b6c0bcbf4dc88b03a66a749b7db02d2c03dda1b4
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-27 13:37:15 -0300

    corrigi a rota de alterar senha.

--- src/app/routes/usuario.routes.js
+++ src/app/routes/usuario.routes.js
@@ -9,7 +9,7 @@ router.post("/usuario", usuarioController.create);
 router.post("/usuario/login",usuarioController.login);
 router.delete("/usuario/:id", authMiddleware, usuarioController.delete);
 router.patch("/usuario/:id", authMiddleware, usuarioController.update);
-router.patch("/usuario/:id/senha", usuarioController.updateSenha);
+router.patch("/usuario/:id/senha", authMiddleware, usuarioController.updateSenha);
 
     
 export default router;
