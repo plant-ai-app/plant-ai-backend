e0a4113e4d6772dbe71be010f14887188db8f68a
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-29 13:40:42 -0300

    ✨ :feat:  Adiciona a classe PasswordService mais método

---
+++ src/app/services/password.service.js
@@ -0,0 +1,34 @@
+import crypto from 'crypto';
+import bcrypt from 'bcryptjs';
+import passwordRepository from '../repositories/password.repository';
+import emailService from '../../infra/email/email.service.js';
+import e from 'express';
+
+class PasswordService {
+    async forgotPassword(email) {
+        const usuario = await passwordRepository.findUserByEmail(email);
+
+        if (!usuario) return;
+
+        await passwordRepository.invalidateUserTokens(usuario.id);
+
+        const token = crypto.randomBytes(32).toString('hex');
+        const tokenHash = await bcrypt.hash(token, 10);
+
+        const expriraEm = new Date(Date.now() + 15 * 60 * 1000); // 15 minutos
+
+        await passwordRepository.createToken({
+            fk_usuario_id: usuario.id,
+            token_hash: tokenHash,
+            expira_em: expriraEm
+        })
+
+        // link por enquato
+        const link = `http://localhost:3000/reset-password?token=${token}`;
+        console.log(`Link de reset de senha: ${link}`);
+
+
+    }
+}
+
+export default new PasswordService();
