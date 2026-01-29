a91ea276230f4e4c122b8ccc65e5265a27ca19b2
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-29 13:58:21 -0300

    🐛 :bug: Correção na importação de arquivo externo e na exportação do arquivo

--- src/app/routes/password.routes.js
+++ src/app/routes/password.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { authMiddleware } from "../middlewares/authMiddleware";
 
 import passwordController from "../controllers/password.controller.js";
 
 const router = Router();
 
 router.post("/password/forgot", passwordController.forgot);
+
+export default router;
