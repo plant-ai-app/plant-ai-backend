27b8bc161d743c1ee7bc48cd7b8d68eb510a76e4
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-29 20:06:40 -0300

    Atualização em método

--- src/app/controllers/password.controller.js
+++ src/app/controllers/password.controller.js
@@ -9,7 +9,7 @@ class passwordController{
 
             return res.status(200).json({message: "Se o e-mail existir em nossa base, um link de recuperação de senha será enviado."});
         } catch (error) {
-            return res.status(500).json({ error: "Erro ao processar solicitação." });
+           return res.status(500).json({message: error.message});
         }
     }
 }
