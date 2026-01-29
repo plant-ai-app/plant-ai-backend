6250730a5e932eb831e88f1d45142551db783b6e
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-29 13:37:52 -0300

    ✨ :feat: Adiciona novo método na classe

--- src/app/controllers/password.controller.js
+++ src/app/controllers/password.controller.js
@@ -1,6 +1,16 @@
 import passwordService from "../services/password.service";
 
 class passwordController{
+    forgot = async (req, res) => {
+        try {
+            const {email} = req.body;
 
+            await passwordService.forgotPassword(email);
+
+            return res.status(200).json({message: "Se o e-mail existir em nossa base, um link de recuperação de senha será enviado."});
+        } catch (error) {
+            return res.status(500).json({ error: "Erro ao processar solicitação." });
+        }
+    }
 }
 export default new passwordController();
