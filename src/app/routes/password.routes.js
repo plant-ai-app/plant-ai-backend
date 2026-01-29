257e44ba647ffad7565f5018aecfa10ae4966e23
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-29 20:07:44 -0300

    ✨ :feat: Adição de nova rota

--- src/app/routes/password.routes.js
+++ src/app/routes/password.routes.js
@@ -5,5 +5,6 @@ import passwordController from "../controllers/password.controller.js";
 const router = Router();
 
 router.post("/password/forgot", passwordController.forgot);
+router.post("/password/reset", passwordController.reset);
 
 export default router;
