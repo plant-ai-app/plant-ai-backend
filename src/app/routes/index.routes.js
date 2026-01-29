bf98cd08d854fb5fe23d25fd9e32989b9a77ec7d
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-29 13:46:59 -0300

    ✨ :feat:  Adiciona novo arquivo de rotas ao index.routes

--- src/app/routes/index.routes.js
+++ src/app/routes/index.routes.js
@@ -1,5 +1,7 @@
 import usuarioRoutes from './usuario.routes.js';
+import passwordRoutes from './password.routes.js';
 
 export const Routes = (app) =>{
     app.use('/api', usuarioRoutes);
+    app.use('/api', passwordRoutes);
 }
