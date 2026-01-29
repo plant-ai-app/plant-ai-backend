0c4d0197bec1ab0c7a3394c11275a11643c9d9e6
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-01-29 13:38:58 -0300

    ✨ :feat: adiciona a classe PasswordRepository mais métodos

---
+++ src/app/repositories/password.repository.js
@@ -0,0 +1,33 @@
+import { prisma } from "../../databases/prisma.js";
+
+class PasswordRepository {
+    
+  async findUserByEmail(email) {
+    return prisma.usuario.findUnique({
+      where: { email }
+    });
+  }
+
+  // Invalidar tokens antigos (encerrar_em != null)
+  async invalidateUserTokens(usuarioId) {
+    return prisma.pwd_reset_token.updateMany({
+      where: {
+        fk_usuario_id: usuarioId,
+        encerrado_em: null
+      },
+      data: {
+        encerrado_em: new Date()
+      }
+    });
+  }
+
+  async createToken(data) {
+    return prisma.pwd_reset_token.create({ data });
+  }
+
+  async findActiveToken(){
+    return prisma.pwd
+  }
+}
+
+export default new PasswordRepository();
