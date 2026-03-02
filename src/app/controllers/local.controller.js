df349f435cd822eeac26e8f8c6a61d4d7139147d
Bruno Lima Beserra <bruno.beserra2024@gmail.com>
2026-03-02 16:48:27 -0300

    ✨ :feat: Adiciona novo controller - local.controller.

---
+++ src/app/controllers/local.controller.js
@@ -0,0 +1,19 @@
+class LocalController {
+    async store(req, res) {
+        
+    }
+
+    async findAll(req, res) {
+        
+    }
+
+    async update(req, res) {
+        
+    }
+
+    async delete(req, res) {
+        
+    }
+}
+
+export default new LocalController();
