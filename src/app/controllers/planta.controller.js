7df39caa66ba676213e5649d6f6eba0e7d893518
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-10 13:05:02 -0300

    ✨ :feat: Criação do método create em planta.controller.

--- src/app/controllers/planta.controller.js
+++ src/app/controllers/planta.controller.js
@@ -1,6 +1,36 @@
+import plantaService from "../services/planta.service.js";
+
 class PlantaController {
-    async store(req, res) {
-        
+    
+    async create(req, res) {
+        try {
+            const {
+                fk_local_id,
+                nome_popular,
+                nome_cientifico,
+                apelido,
+                foto_url,
+                data_aquisicao,
+                observacao,
+            } = req.body;
+
+            const data = {
+               fk_usuario_id: req.usuarioId,
+               fk_local_id,
+               nome_popular,
+               nome_cientifico,
+               apelido,
+               foto_url,
+               data_aquisicao,
+               observacao,
+            }
+
+            const planta = await plantaService.create(data);
+          
+            return res.status(201).json({ message: "Planta criada com sucesso.", planta });
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
     }
 }
 
