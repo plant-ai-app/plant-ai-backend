d384ed8695a92badc74edadbd49cdce6dfbd22ac
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-10 13:24:13 -0300

    ✨ :feat: Adiciona novas rotas a planta.routes.

--- src/app/routes/planta.routes.js
+++ src/app/routes/planta.routes.js
@@ -5,5 +5,8 @@ import plantaController from "../controllers/planta.controller.js";
 const router = Router();
 
 router.post("/planta", authMiddleware, plantaController.create);
+router.get("/plantas", authMiddleware, plantaController.findAll);
+router.get("/planta/:id", authMiddleware, plantaController.findById);
+router.get("/plantas/usuario/:id", authMiddleware, plantaController.findByUserId);
 
 export default router
