498ffd45710dcdb015a1f7c94756db5b61e0cf59
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-10 13:07:18 -0300

    ♻️ :refactor: Melhora o middleware de validação.

--- src/app/middlewares/authMiddleware.js
+++ src/app/middlewares/authMiddleware.js
@@ -7,7 +7,13 @@ export const authMiddleware = (req, res, next) => {
         return res.status(401).json({ error: 'Token não fornecido.' });
     }
 
-    const [, token] = authHeader.split(' ');
+    const parts = authHeader.split(' ');
+
+    if (parts.length !== 2) {
+        return res.status(401).json({ error: 'Token mal formatado.' });
+    }
+
+    const [, token] = parts;
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
