4b0da969735b68dbff7e90be54d58097632d2dc0
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-10 13:06:05 -0300

    ✨ :feat: Criação do método findAll em local.controller.

--- src/app/controllers/local.controller.js
+++ src/app/controllers/local.controller.js
@@ -1,10 +1,17 @@
+import localService from "../services/local.service.js";
+
 class LocalController {
     async store(req, res) {
         
     }
 
     async findAll(req, res) {
-        
+        try {
+            const locais = await localService.findAll();
+            return res.status(200).json({message: 'Locais encontrados', locais});
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
     }
 
     async update(req, res) {
