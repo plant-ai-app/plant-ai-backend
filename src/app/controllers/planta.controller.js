973052148ba174b828372795b0e88802798cd995
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-10 13:22:34 -0300

    ✨ :feat: Criação de métodos finds em planta.controller.

--- src/app/controllers/planta.controller.js
+++ src/app/controllers/planta.controller.js
@@ -32,6 +32,34 @@ class PlantaController {
             return res.status(400).json({ message: error.message });
         }
     }
+
+    async findAll(req, res) {
+        try {
+            const plantas = await plantaService.findAll();
+            return res.status(200).json({message: "Plantas encontradas com sucesso.", plantas});
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    async findByUserId(req, res) {
+        try {
+            const plantas = await plantaService.findByUserId(req.usuarioId);
+            return res.status(200).json({message: "Plantas encontradas com sucesso.", plantas});
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    async findById(req, res) {
+        try {
+            const planta = await plantaService.findById(req.params.id);
+            return res.status(200).json({message: "Planta encontrada com sucesso.", planta});
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
 }
 
 export default new PlantaController();
