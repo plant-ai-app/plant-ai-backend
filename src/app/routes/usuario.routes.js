88d9d678c0ac502e69b64e9dbdc23a2f88152a26
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-18 17:38:09 -0300

    ♻️ :refactor: Refatora url do enpoint.

--- src/app/routes/usuario.routes.js
+++ src/app/routes/usuario.routes.js
@@ -5,11 +5,12 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.get("/usuarios", usuarioController.findAll);
+router.get("/usuario/me", authMiddleware, usuarioController.findById);
 router.post("/usuario", usuarioController.create);
 router.post("/usuario/login",usuarioController.login);
 router.patch("/usuario/senha", authMiddleware, usuarioController.updateSenha);
 router.delete("/usuario", authMiddleware, usuarioController.delete);
-router.patch("/usuario/:id", authMiddleware, usuarioController.update);
+router.patch("/usuario", authMiddleware, usuarioController.update);
 
     
 export default router;
