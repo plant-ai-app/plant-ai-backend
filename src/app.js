e7edff4ca3052ec5280a9df8bd585dd7c9e1f900
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-19 20:58:03 -0300

    ✨ :feat: Adiciona implementação para ler arquivos do projeto.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import path from 'path';
 import {Routes} from './app/routes/index.routes.js';
 
 const app = express();
+app.use("/uploads", express.static(path.resolve("uploads")));
 app.use(express.json());
 
 app.use(cors({
