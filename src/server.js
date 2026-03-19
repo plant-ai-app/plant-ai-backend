fb911dd35249ac86c3048a6aef17fe2930fed76b
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-19 20:59:52 -0300

    ⚡️:perf: Melhora a forma de utilizar o Adress.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import app from './app.js'
 
 const PORT = process.env.PORT
-const Adress = 'localhost'
+const Adress = process.env.ADRESS || 'localhost'
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://${Adress}:${PORT}/api`)
