8bb1e510fbea588dbbc8087fab917ed482f2cfbb
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-19 21:03:46 -0300

    ♻️ :refactor: Adiciona novas rotas - avatar.routes

--- src/app/routes/index.routes.js
+++ src/app/routes/index.routes.js
@@ -2,10 +2,13 @@ import usuarioRoutes from './usuario.routes.js';
 import passwordRoutes from './password.routes.js';
 import localRoutes from './local.routes.js';
 import plantaRoutes from './planta.routes.js';
+import avatarRoutes from './avatar.routes.js';
+
 
 export const Routes = (app) =>{
     app.use('/api', usuarioRoutes);
     app.use('/api', passwordRoutes);
     app.use('/api', localRoutes);
     app.use('/api', plantaRoutes);
+    app.use('/api', avatarRoutes);
 }
