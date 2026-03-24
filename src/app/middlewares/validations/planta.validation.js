d0937bb7eb40782699e1a3915a43372091f12dbb
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-24 14:59:19 -0300

    🔥: remove: realoca arquivos.

--- src/app/middlewares/validations/planta.validation.js
+++
@@ -1,8 +0,0 @@
-export const validarCriacaoPlanta = (data) => {
-    const { nome_popular, nome_cientifico, apelido, foto_url, data_aquisicao, observacao } = data;
-    
-    if(nome_popular.trim().length < 3){
-        throw new Error("Nome popular deve ter no mínimo 3 caracteres.");
-    }
-
-}
