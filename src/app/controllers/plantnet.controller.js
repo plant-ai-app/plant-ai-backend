7760ded96d0f4b0dae41140bf78ed9999eb32259
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-03-26 16:48:35 -0300

    ✨ :feat: Adiciona controller para identificação -  Pl@ntNet.

---
+++ src/app/controllers/plantnet.controller.js
@@ -0,0 +1,21 @@
+import plantnetService from "../services/plantnet.service.js";
+
+class PlantnetController {
+    
+    async identify(req, res) {
+        try {
+            // Este endpoint pode receber a imagem ou a url da imagem
+            // para enviar para identificação no Pl@ntNet.
+            const plants = await plantnetService.identify(req);
+          
+            return res.status(200).json({ message: "Identificação realizada com sucesso.", plants });
+        } catch (error) {
+            // Se o erro for de "não encontrada", podemos retornar um 404, senão 400.
+            const status = error.message.includes("Não foi possível") ? 404 : 400;
+            return res.status(status).json({ message: error.message, plants: [] });
+        }
+    }
+
+}
+
+export default new PlantnetController();
