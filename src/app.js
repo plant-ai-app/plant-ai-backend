6e96f43b5b467970459b968e402712ab2d42f6bb
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-10 15:49:09 -0300

    ♻️ :refactor: Mudanças no arquivo app.js.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import testarConexao from '../teste-gemini.js';
 
 const app = express();
 app.use("/uploads", express.static(path.resolve("uploads")));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
     origin: true,
