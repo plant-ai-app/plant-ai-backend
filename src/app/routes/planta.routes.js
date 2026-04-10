33bc840ac244fc3155a5e5d728fcf14198cf55f6
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-10 15:48:48 -0300

    ♻️ :refactor: Mudança na rota de post.

--- src/app/routes/planta.routes.js
+++ src/app/routes/planta.routes.js
@@ -4,7 +4,7 @@ import plantaController from "../controllers/planta.controller.js";
 
 const router = Router();
 
-router.post("/planta", authMiddleware, plantaController.create);
+router.post(["/planta", "/plant"], authMiddleware, plantaController.create);
 router.get("/plantas", plantaController.findAll);
 router.get("/plantas/usuario", authMiddleware, plantaController.findByUserId);
 router.get("/planta/:id", plantaController.findById);
