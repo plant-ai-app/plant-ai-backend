33640867d066b4db52a2ff83ba3bf0cce2c65ea6
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-13 15:24:57 -0300

    ♻️ :refactor: Adiciona novas rotas ao index.routes.js

--- src/app/routes/index.routes.js
+++ src/app/routes/index.routes.js
@@ -4,6 +4,8 @@ import localRoutes from './local.routes.js';
 import plantaRoutes from './planta.routes.js';
 import avatarRoutes from './avatar.routes.js';
 import plantnetRoutes from './plantnet.routes.js';
+import cuidadoTipoRoutes from './cuidadoTipo.routes.js';
+import cuidadoRoutes from './cuidado.routes.js';
 
 
 export const Routes = (app) =>{
@@ -13,4 +15,6 @@ export const Routes = (app) =>{
     app.use('/api', plantaRoutes);
     app.use('/api', avatarRoutes);
     app.use('/api', plantnetRoutes);
+    app.use('/api', cuidadoTipoRoutes);
+    app.use('/api', cuidadoRoutes);
 }
