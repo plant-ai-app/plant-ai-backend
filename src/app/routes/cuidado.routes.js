4f4430a11af6eb0a07945066a20500a097487b5d
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-13 15:58:01 -0300

    ♻️ :refactor: Pequena correção nas rotas.

--- src/app/routes/cuidado.routes.js
+++ src/app/routes/cuidado.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 import cuidadoController from "../controllers/cuidado.controller.js";
 
 const router = Router();
 
-router.post("/cuidados", cuidadoController.create);
-router.get("/cuidados", cuidadoController.findAll);
-router.get("/cuidados/:id", cuidadoController.findById);
-router.get("/cuidados/planta/:plantaId", cuidadoController.findByPlantaId);
-router.put("/cuidados/:id", cuidadoController.update);
-router.delete("/cuidados/:id", cuidadoController.delete);
+router.post("/cuidado", authMiddleware, cuidadoController.create);
+router.get("/cuidado", authMiddleware, cuidadoController.findAll);
+router.get("/cuidado/:id", authMiddleware, cuidadoController.findById);
+router.get("/cuidado/planta/:plantaId", authMiddleware, cuidadoController.findByPlantaId);
+router.patch("/cuidado/:id", authMiddleware, cuidadoController.update);
+router.delete("/cuidado/:id", authMiddleware, cuidadoController.delete);
 
 export default router;
