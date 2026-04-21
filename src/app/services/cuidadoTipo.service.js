c0088e9e70ea05c7acefe405b4e0794c10b94931
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-20 23:25:49 -0300

    ♻️ :refactor: Adiciona método findById em CuidadoTipoService.

--- src/app/services/cuidadoTipo.service.js
+++ src/app/services/cuidadoTipo.service.js
@@ -4,6 +4,14 @@ class CuidadoTipoService {
     async findAll() {
         return await cuidadoTipoRepository.findAll();
     }
+
+    async findById(id) {
+        const cuidadoTipo = await cuidadoTipoRepository.findById(id);
+        if (!cuidadoTipo) {
+            throw new Error("Tipo de cuidado não encontrado.");
+        }
+        return cuidadoTipo;
+    }
 }
 
 export default new CuidadoTipoService();
