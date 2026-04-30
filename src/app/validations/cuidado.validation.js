12fb2adb396fa396e081e590e70b9916afc2e59b
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-04-29 22:59:21 -0300

    ♻️ :refactor: exporta novas validações.

--- src/app/validations/cuidado.validation.js
+++ src/app/validations/cuidado.validation.js
@@ -6,3 +6,30 @@ export const validateUniqueCareType = async (planta_id, tipo_id) => {
         throw new Error("Esta planta já possui este tipo de cuidado cadastrado.");
     }
 }
+
+export const validateCuidadoCreate = async (data) => {
+    if (!data.planta_id) {
+        throw new Error("O campo 'planta_id' é obrigatório.");
+    }
+    if (!data.tipo_id) {
+        throw new Error("O campo 'tipo_id' é obrigatório.");
+    }
+    if (!data.frequencia_dias) {
+        throw new Error("O campo 'frequencia_dias' é obrigatório.");
+    }
+    if (!data.proxima_data) {
+        throw new Error("O campo 'proxima_data' é obrigatório.");
+    }
+
+    if (data.quantidade_instrucao && data.quantidade_instrucao.length > 80) {
+        throw new Error("O campo 'quantidade_instrucao' não pode exceder 80 caracteres.");
+    }
+
+    await validateUniqueCareType(data.planta_id, data.tipo_id);
+}
+
+export const validateCuidadoUpdate = (data) => {
+    if (data.quantidade_instrucao && data.quantidade_instrucao.length > 80) {
+        throw new Error("O campo 'quantidade_instrucao' não pode exceder 80 caracteres.");
+    }
+}
