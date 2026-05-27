aabf4311997bfa138130a21c9022b8526db70706
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-05-26 22:56:32 -0300

    ♻️ :refactor: Testa conexão com o novo banco.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import 'dotenv/config'
 import app from './app.js'
+import { connectMongo } from './databases/mongoose.js';
 
 const PORT = process.env.PORT
 const Adress = process.env.ADRESS || 'localhost'
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://${Adress}:${PORT}/api`)
-})
+
+const StartServer = async () => {
+
+  await connectMongo();
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://${Adress}:${PORT}/api`)
+  })
+
+};
+
+StartServer();
