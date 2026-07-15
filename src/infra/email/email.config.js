9625a4dc5d396456496d551ad995b449e29673b4
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-07-14 22:02:20 -0300

    ♻️  :fix: Adiciona configuração para forçar o uso de IPv4 no transporte de e-mail para evitar lentidão e timeouts de DNS.

--- src/infra/email/email.config.js
+++ src/infra/email/email.config.js
@@ -7,6 +7,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
   secure: false, // true só se for porta 465
+  family: 4, // Força o uso de IPv4 para evitar lentidão/timeouts de DNS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
