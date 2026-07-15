d94182bb81cc7e9441965429e57b0be27fa5d434
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2026-07-14 22:01:51 -0300

    ✨ :feat: Adiciona origem ao método de recuperação de senha para melhorar a funcionalidade.

--- src/app/controllers/password.controller.js
+++ src/app/controllers/password.controller.js
@@ -3,9 +3,10 @@ import passwordService from "../services/password.service.js";
 class passwordController{
     forgot = async (req, res) => {
         try {
-            const {email} = req.body;
+            const { email } = req.body;
+            const origin = req.headers.origin || req.headers.referer;
 
-            await passwordService.forgotPassword(email);
+            await passwordService.forgotPassword(email, origin);
 
             return res.status(200).json({message: "Se o e-mail existir em nossa base, um link de recuperação de senha será enviado."});
         } catch (error) {
